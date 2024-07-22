5092dfaa97f4e62afac277c60c08710cab40eacd
Tran Xuan Vu <141942017+VuTXdata@users.noreply.github.com>
2024-07-22 23:27:33 +0700

    update 22.07.2024
    Học về JPA Responsitory

--- Section4_REST_CRUD API/Lesson2_SpringBootRestProject/cruddemo/cruddemo/src/main/java/com/vuhocspring/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ Section4_REST_CRUD API/Lesson2_SpringBootRestProject/cruddemo/cruddemo/src/main/java/com/vuhocspring/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+// Sử dụng JpaReponsitory thì không cần triển khai class Impl này nữa
 @Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO{
 
